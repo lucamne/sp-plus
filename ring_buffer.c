23d7de3a292b58ec7c2350da1eede5eb8c95ea81
Luca <negrislmn@gmail.com>
2025-01-06 00:06:09 -0500

    Fixed bugs with ring buffer.

--- ring_buffer.c
+++ ring_buffer.c
@@ -24,10 +24,13 @@ int write_to_buf(struct ring_buf* rb, const void* src, int size)
 
 	// number of bytes before wrap is needed
 	const int n = rb->data_size - rb->head; 
-	memcpy(rb->data + rb->head, src, n);
 	// wrap if needed
-	if (n < size) 
-		memcpy(rb->data, src + n, size - n);
+	if (n >= size) {
+		memcpy(rb->data + rb->head, src, size);
+	} else if (n < size) {
+		memcpy(rb->data + rb->head, src, n);
+		memcpy(rb->data, (char *) src + n, size - n);
+	}
 
 	rb->head = (rb->head + size) % rb->data_size;
 	rb->in_use += size;
@@ -42,10 +45,13 @@ int read_to_dest(struct ring_buf* rb, void* dest, int size)
 	const int read = (rb->head + (rb->data_size - rb->in_use)) % rb->data_size;
 	// number of bytes before wrap is needed
 	const int n = rb->data_size - read;
-	memcpy(rb->data + read, dest, n);
 	// wrap if needed
-	if (n < size)
-		memcpy(rb->data, dest + n, size - n);
+	if (n >= size) {
+		memcpy(dest, rb->data + read, size);
+	} else if (n < size) {
+		memcpy(dest, rb->data + read, n);
+		memcpy((char *) dest + n, rb->data, size - n);
+	}
 
 	rb->in_use -= size;
 	return size;
--- sp-plus.c
+++ sp-plus.c
@@ -2,7 +2,9 @@
 #include "sample.h"
 #include "ring_buffer.h"
 
+#include <assert.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 #define TEST_WAV "test/StarWars.wav"
 
@@ -11,16 +13,37 @@ int main(int argc, char** argv)
 	struct sample* samp = init_sample();
 	load_wav_into_sample(TEST_WAV, samp);
 
-	struct ring_buffer* rb = init_ring_buf(2000); 
+	struct ring_buf* rb = init_ring_buf(129837); 
 	if (!rb)
 		printf("Error initializing ring buffer");
 
 	char* tmp = calloc(1, samp->data_size);
-	const int transfer_size = samp->frame_size;
-	while (1) {
-		
+	const int transfer_size = 11;
+	int bytes_sent = 0;
+	int bytes_received = 0;
+	int _t;
+	printf("Frame size: %d\n", transfer_size);
 
-	play_clip(samp->data, samp->data_size);
+	while (bytes_sent + transfer_size <= samp->data_size) {
+		while (bytes_sent + transfer_size <= samp->data_size) {
+			_t = write_to_buf(rb, samp->data + bytes_sent, transfer_size); 
+			bytes_sent += _t;
+			if (!_t)
+				break;
+		}
+		while ((_t = read_to_dest(rb, tmp + bytes_received, transfer_size)) != 0)
+			bytes_received += _t; 
+
+		//assert(tmp[bytes_received - 1] == samp->data[bytes_received - 1]);
+
+	}
+	for (int i = 0; i < bytes_sent; i++)
+	{
+		assert(tmp[i] == samp->data[i]);
+	}
+	printf("Original size: %d\n", samp->data_size);
+	printf("Bytes transferred: %d\n", bytes_sent);
+	play_clip(tmp, bytes_sent);
 
 	return 0;
 }
